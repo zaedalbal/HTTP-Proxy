3ee8eb4a3f48efce89be2bd66622629e9c3f2027
zaedalbal <vokiolhol@gmail.com>
2026-01-24 19:27:30 +0300

    переписал http_handler теперь при http соеденении парсится только первый запрос, а потом прокси превращается в тунель

--- src/network/session.cpp
+++ src/network/session.cpp
@@ -81,33 +81,15 @@ boost::asio::awaitable<void> Session::send_bad_request(const std::string str)
 boost::asio::awaitable<void> Session::http_handler
 (const std::string& host, const std::string& port, const boost::beast::http::request<boost::beast::http::string_body> request)
 {
-    auto executor = client_socket_.get_executor(); // получение executor'а
+    auto executor = client_socket_.get_executor();
     boost::asio::ip::tcp::resolver resolver(executor);
     boost::system::error_code ec;
-    auto upstream_ptr = std::make_shared<boost::asio::ip::tcp::socket>(executor); // сокет для соеденения с сервером
-    auto finished = std::make_shared<std::atomic_bool>(false); // флаг завершения
+    auto upstream_ptr = std::make_shared<boost::asio::ip::tcp::socket>(executor);
+    auto finished = std::make_shared<std::atomic_bool>(false);
     auto self_weak = weak_from_this();
     auto timer = std::make_shared<Timer>(executor, __PROXY_GLOBALS__::PROXY_CONFIG.timeout_milliseconds);
-    timer->set_callback_func([upstream_ptr](){upstream_ptr->close();}); // колбэк для коннетка к серверу и резолвинга
-    timer->start(); // старт таймера
-    auto results = co_await resolver.async_resolve(
-        host, port, 
-        boost::asio::redirect_error(boost::asio::use_awaitable, ec)); 
-    timer->refresh();
-    co_await boost::asio::async_connect( // подключение к серверу
-        *upstream_ptr, results, 
-        boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-    timer->refresh();
-    if(ec)
-    {
-        timer->stop();
-#ifdef DEBUG
-        DEBUG_LOGGER << "Error in connect to upstream: " << ec.what() << std::endl;
-#endif
-        co_await send_bad_request(ec.what());
-        co_return;
-    }
-    auto close_both = [self_weak, upstream_ptr, timer]() // закрытие обоих сокетов
+    
+    auto close_both = [self_weak, upstream_ptr, timer]()
     {
         if(auto self = self_weak.lock())
         {
@@ -118,131 +100,154 @@ boost::asio::awaitable<void> Session::http_handler
             self->client_socket_.close(ec);
             upstream_ptr->close(ec);
         }
-    };
-    const size_t MAX_BODY_SIZE = 1024 * 1024 * 64; // лимит телв 64мб
-    if(request.body().size() > MAX_BODY_SIZE) // если тело больше чем 64мб, послать BAD REQUEST и закрыть сокеты
-    {
-#ifdef DEBUG
-        DEBUG_LOGGER << "Error: HTTP request body too large!" << std::endl;
-#endif
-        co_await send_bad_request("Error: HTTP request too large!");
-        close_both();
-        co_return;
-    }
-    
-    auto request_str = std::make_shared<std::string>();
-    std::ostringstream oss;
-    oss << request;             // преобразование запроса в строку,
-    *request_str = oss.str();   // чтобы можно было контроллировать скольк байт передается
-    
-    // корутина для отправки запроса от клиента к серверу
-    auto client_to_server = [self_weak, upstream_ptr, finished, close_both, request_str, timer]() -> boost::asio::awaitable<void>
-    {
-        if(auto self = self_weak.lock())
-        {
-            boost::system::error_code ec;
-            auto bytes_transferred = request_str->size(); // размер в байтах всего request'а
-            std::size_t offset = 0; // смещение в буфере
-            while(offset < bytes_transferred)
-            {
-                auto allowed = self->traffic_limiter_->acquire(bytes_transferred - offset);
-                if(allowed == 0) // ждать 10мс пока токены не обновятся
-                {
-                    boost::asio::steady_timer timer(self->client_socket_.get_executor());
-                    timer.expires_after(std::chrono::milliseconds(10));
-                    co_await timer.async_wait(boost::asio::use_awaitable);
-                    continue;
-                }
-                auto sent = co_await boost::asio::async_write(
-                    *upstream_ptr,
-                    boost::asio::buffer(request_str->data() + offset, allowed),
-                    boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-                timer->refresh();
-                if(ec)
-                    break;
-                offset += sent;
-            }
-        
-#ifdef DEBUG
-        if(ec)
-            DEBUG_LOGGER << "Error in client_to_server: " << ec.what() << std::endl;
-#endif
-            timer->stop();
-            if(!finished->exchange(true))
-                close_both();
-        }
+        else
+            return;
     };
 
-    // корутина для отправки ответа клиенту от сервера
+    timer->set_callback_func([upstream_ptr](){upstream_ptr->close();});
+    timer->start();
+    auto results = co_await resolver.async_resolve(host, port, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+    timer->refresh();
+    co_await boost::asio::async_connect(*upstream_ptr, results, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+    timer->refresh();
+    if(ec)
+    {
+        timer->stop();
+#ifdef DEBUG
+        __PROXY_GLOBALS__::DEBUG_LOGGER << "Error in connect to upstream: " << ec.what() << std::endl;
+#endif
+        co_await send_bad_request(ec.what());
+        co_return;
+    }
+
+    auto modified_req = request; // модификация запроса для upstream сервера
+    std::string target = std::string(request.target()); // конвертация url
+    auto scheme_pos = target.find("://");
+    if(scheme_pos != std::string::npos)
+    {
+        auto path_pos = target.find('/', scheme_pos + 3);
+        if(path_pos != std::string::npos)
+            target = target.substr(path_pos);
+        else
+            target = "/";
+    }
+    modified_req.target(target);
+    modified_req.erase(boost::beast::http::field::proxy_connection); // удаление proxy-connection заголовка
+
+    // отправка модифицированного запроса на upstream сервер
+    co_await boost::beast::http::async_write(*upstream_ptr, modified_req, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+    timer->refresh();
+    if(ec)
+    {
+        timer->stop();
+#ifdef DEBUG
+        __PROXY_GLOBALS__::DEBUG_LOGGER << "Error writing to upstream: " << ec.what() << std::endl;
+#endif
+        co_await send_bad_request(ec.what());
+        co_return;
+    }
+    timer->set_callback_func([finished](){finished->store(true);});
+
+    // корутина для чтения от сервера и отправки клиенту
     auto server_to_client = [self_weak, upstream_ptr, finished, close_both, timer]() -> boost::asio::awaitable<void>
     {
         if(auto self = self_weak.lock())
         {
+            std::array<char, TUNNEL_BUFFER_SIZE> read_buffer_from_server;
             boost::system::error_code ec;
-            boost::beast::flat_buffer read_buffer;
-            boost::beast::http::response<boost::beast::http::string_body> res;
-
-            co_await boost::beast::http::async_read(
-                *upstream_ptr, 
-                read_buffer, 
-                res,
-                boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-            timer->refresh();
-            if(ec)  
+            for(;;)
             {
-#ifdef DEBUG
-                DEBUG_LOGGER << "Error reading response from upstream: " << ec.what() << std::endl;
-#endif
-                timer->stop();
-                if(!finished->exchange(true))
-                co_return;
-            }
-            const size_t MAX_BODY_SIZE = 1024 * 1024 * 64; // лимит тела 64мб
-            if(res.body().size() > MAX_BODY_SIZE) // если тело больше чем 64мб, отправить BAD REQUEST
-            {
-#ifdef DEBUG
-        __PROXY_GLOBALS__::DEBUG_LOGGER << "Error: HTTP response body too large!" << std::endl;
-#endif
-                timer->stop();
-                if(!finished->exchange(true))
-                co_return;
-            }
-            auto response_str = std::make_shared<std::string>();
-            std::ostringstream oss;
-            oss << res;                 // преобразование в ответа в строку,
-            *response_str = oss.str();  // чтобы можно было контроллировать сколько байт передается
-            auto bytes_transferred = response_str->size(); // размер в байтах всего ответа
-            std::size_t offset = 0; // смещение в буфере
-            while(offset < bytes_transferred)
-            {
-                auto allowed = self->traffic_limiter_->acquire(bytes_transferred - offset);
-                if(allowed == 0) // ждать 10мс пока токены не обновсятся
-                {
-                    boost::asio::steady_timer timer(self->client_socket_.get_executor());
-                    timer.expires_after(std::chrono::milliseconds(10));
-                    co_await timer.async_wait(boost::asio::use_awaitable);
-                    continue;
-                }
-                auto sent = co_await boost::asio::async_write(
-                    self->client_socket_,
-                    boost::asio::buffer(response_str->data() + offset, allowed),
-                    boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-                timer->refresh();
-                if(ec)
+                if(finished->load())
                     break;
-                offset += sent;
+                auto bytes_transferred = co_await upstream_ptr->async_read_some
+                (boost::asio::buffer(read_buffer_from_server), boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+                timer->refresh();
+                if(bytes_transferred == 0 || ec)
+                    break;
+                std::size_t offset = 0;
+                while(offset < bytes_transferred)
+                {
+                    auto allowed = self->traffic_limiter_->acquire(bytes_transferred - offset);
+                    if(allowed == 0)
+                    {
+                        boost::asio::steady_timer wait_timer(self->client_socket_.get_executor());
+                        wait_timer.expires_after(std::chrono::milliseconds(10));
+                        co_await wait_timer.async_wait(boost::asio::use_awaitable);
+                        continue;
+                    }
+                    
+                    auto sent = co_await boost::asio::async_write
+                    (self->client_socket_,
+                    boost::asio::buffer(read_buffer_from_server.data() + offset, allowed),
+                    boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+                    timer->refresh();
+                    
+                    if(ec)
+                        break;
+                    offset += sent;
+                }
             }
+            
 #ifdef DEBUG
-    if(ec)
-        __PROXY_GLOBALS__::DEBUG_LOGGER << "Error in server_to_client: " << ec.what() << std::endl;
+            if(ec)
+                __PROXY_GLOBALS__::DEBUG_LOGGER << "Error in server_to_client (HTTP): " << ec.what() << std::endl;
 #endif
-            timer->stop();
-            if(!finished->exchange(true)) // если данная корутина завершилась первой, то закрыть сокеты
+            if(!finished->exchange(true))
                 close_both();
         }
+        else
+            co_return;
     };
-    timer->set_callback_func([finished](){finished->store(true);}); // колбэк для корутин
-    co_await (boost::asio::experimental::awaitable_operators::operator&&(client_to_server(), server_to_client())); // запуск корутин
+
+    // корутина для чтения от клиента и отправки серверу
+    auto client_to_server = [self_weak, upstream_ptr, finished, close_both, timer]() -> boost::asio::awaitable<void>
+    {
+        if(auto self = self_weak.lock())
+        {
+            std::array<char, TUNNEL_BUFFER_SIZE> read_buffer_from_client;
+            boost::system::error_code ec;
+            for(;;)
+            {
+                if(finished->load())
+                    break;
+                auto bytes_transferred = co_await self->client_socket_.async_read_some
+                (boost::asio::buffer(read_buffer_from_client), boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+                timer->refresh();
+                if(bytes_transferred == 0 || ec)
+                    break;
+                std::size_t offset = 0;
+                while(offset < bytes_transferred)
+                {
+                    auto allowed = self->traffic_limiter_->acquire(bytes_transferred - offset);
+                    if(allowed == 0)
+                    {
+                        boost::asio::steady_timer wait_timer(self->client_socket_.get_executor());
+                        wait_timer.expires_after(std::chrono::milliseconds(10));
+                        co_await wait_timer.async_wait(boost::asio::use_awaitable);
+                        continue;
+                    }
+                    auto sent = co_await boost::asio::async_write
+                    (*upstream_ptr,
+                    boost::asio::buffer(read_buffer_from_client.data() + offset, allowed),
+                    boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+                    timer->refresh();
+                    if(ec)
+                        break;
+                    offset += sent;
+                }
+            }
+#ifdef DEBUG
+            if(ec)
+                __PROXY_GLOBALS__::DEBUG_LOGGER << "Error in client_to_server (HTTP): " << ec.what() << std::endl;
+#endif
+            if(!finished->exchange(true))
+                close_both();
+        }
+        else
+            co_return;
+    };
+    // запуск обеих корутин для двунаправленной передачи
+    co_await (boost::asio::experimental::awaitable_operators::operator&&(client_to_server(), server_to_client()));
     co_return;
 }
 
