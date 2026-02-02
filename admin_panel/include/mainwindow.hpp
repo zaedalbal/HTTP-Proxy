5f14843f1f5a632ed023a8d1537a8449ddbd2243
zaedalbal <vokiolhol@gmail.com>
2026-02-02 03:29:43 +0300

    сделал минимальный скелет интерфейса

--- admin_panel/include/mainwindow.hpp
+++ admin_panel/include/mainwindow.hpp
@@ -1,5 +1,21 @@
 #pragma once
 #include <QMainWindow>
+#include <QApplication>
+#include <QWidget>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QTabWidget>
+#include <QLabel>
+#include <QSplitter>
+#include <QGroupBox>
+#include <QMessageBox>
+#include <QCheckBox>
+#include <QListWidget>
+#include <QStackedWidget>
 
 class MainWindow : public QMainWindow
 {
@@ -10,5 +26,20 @@ class MainWindow : public QMainWindow
         ~MainWindow(); // destructor
 
         void setupUI();
+    private:
+        // основное окно
+        QWidget* Central; 
+        QHBoxLayout* CentralLayout;
 
+        // меню
+        QListWidget* Menu; 
+
+        // страницы
+        QStackedWidget* Pages; 
+        QLabel* LogPage;
+        QLabel* ConfigPage;
+        QLabel* ActiveConnectionsPage;
+
+        // сплитер (для изменения размера меню и страниц)
+        QSplitter* Splitter;
 };
--- admin_panel/src/mainwindow.cpp
+++ admin_panel/src/mainwindow.cpp
@@ -23,9 +23,36 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
 
 void MainWindow::setupUI()
 {
-    QWidget* central = new QWidget(this);
-    setCentralWidget(central);
+    // настройка главного окна
+    Central = new QWidget(this);
+    setCentralWidget(Central);
     setWindowTitle("Proxy control panel");
+    CentralLayout = new QHBoxLayout(Central);
+    
+    // настройка меню
+    Menu = new QListWidget;
+    Menu->addItem("Log");
+    Menu->addItem("Config");
+    Menu->addItem("Active connections");
+
+    // настройка страниц
+    Pages = new QStackedWidget;
+    LogPage = new QLabel("LogPage");
+    ConfigPage = new QLabel("ConfigPage");
+    ActiveConnectionsPage = new QLabel("ActiveConnectionsPage");
+    Pages->addWidget(LogPage);
+    Pages->addWidget(ConfigPage);
+    Pages->addWidget(ActiveConnectionsPage);
+
+    // настройка сплиттера
+    connect(Menu, &QListWidget::currentRowChanged, Pages, &QStackedWidget::setCurrentIndex);
+    Splitter = new QSplitter(Qt::Horizontal, Central);
+    Splitter->addWidget(Menu);
+    Splitter->addWidget(Pages);
+    Splitter->setStretchFactor(0, 0);
+    Splitter->setStretchFactor(1, 1);
+    Splitter->setSizes({200, 700});
+    CentralLayout->addWidget(Splitter);
 }
 
 MainWindow::~MainWindow()
