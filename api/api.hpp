9ba02885953dc6bcc609bae992cf967181d7c3ba
zaedalbal <vokiolhol@gmail.com>
2026-02-04 15:58:04 +0300

    сделал api.hpp

---
+++ api/api.hpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <cstdint>
+
+#define MAX_DATA_SIZE 16384
+
+namespace api
+{
+    enum class MessageType
+    {
+        Request,
+        Response,
+        Event
+    };
+
+    enum class CommandName
+    {
+        Proxy_status,
+        Proxy_activeconnections,
+        
+        Config_get,
+
+        Log_get
+    };
+
+    struct SendData
+    {
+        uint32_t data_size;
+        char data[MAX_DATA_SIZE];
+    };
+
+    struct Request
+    {
+        MessageType Type = api::MessageType::Response;
+    };
+
+    struct Response
+    {
+        MessageType Type = api::MessageType::Response;
+        bool ProxyStatus;
+        unsigned short id;
+        CommandName Command;
+        bool isChunckedResponse;
+        SendData data;
+    };
+}
