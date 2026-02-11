0930f0ff6653b6552ae3524ca337fabe7126a9b0
zaedalbal <vokiolhol@gmail.com>
2026-02-11 17:36:44 +0300

    доделал реализацию методов ServerInteraction

--- admin_panel/src/server_interaction/ServerInteraction.cpp
+++ admin_panel/src/server_interaction/ServerInteraction.cpp
@@ -3,6 +3,7 @@
 ServerInteraction::ServerInteraction()
 {
     connect(&socket_, &QTcpSocket::bytesWritten, this, &ServerInteraction::onBytesWritten);
+    connect(&socket_, &QTcpSocket::readyRead, this, &ServerInteraction::onReadyRead);
 }
 
 void ServerInteraction::connectToServer(QString host, quint16 port)
@@ -44,5 +45,39 @@ void ServerInteraction::onBytesWritten(qint64 bytes)
             remainingData_ = remainingData_.mid(written);
         else
             remainingData_.clear(); 
+    }
+}
+
+void ServerInteraction::setResponseHandler(std::function<void(const api::Response)> handler)
+{
+    response_handler_ = handler;
+}
+
+void ServerInteraction::onReadyRead()
+{
+    buffer_.append(socket_.readAll());
+    while(true)
+    {
+        if(buffer_.size() < static_cast<int>(sizeof(api::RequestHeader)))
+            return;
+        api::ResponseHeader header;
+        std::memcpy(&header, buffer_.constData(), sizeof(header));
+        qsizetype total_size = sizeof(api::ResponseHeader) + header.data_size;
+        if(buffer_.size() < total_size)
+            return;
+
+        api::Response response(header.data_size);
+        response.ProxyStatus = header.ProxyStatus;
+        response.RequestFailed = header.RequestFailed;
+        response.id = header.id;
+        response.ResponseCommand = header.ResponseCommand;
+        response.RequestCommand = header.RequestCommand;
+        response.isChunckedResponse = header.isChunckedResponse;
+        response.data_size = header.data_size;
+        if(response.data_size > 0)
+            std::memcpy(response.data.get(), buffer_.constData() + sizeof(api::ResponseHeader), header.data_size);
+        buffer_.remove(0, total_size);
+        if(response_handler_)
+            response_handler_(std::move(response));
     }
 }
